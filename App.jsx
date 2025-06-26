ae52025af43b8db9ac833aa9ca419f22e37b4734
Zaki327 <kzaki3009@gmail.com>
2025-06-26 16:10:20 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">HK Trader Bot</h1>
+      <p>Password protection, SMC overlay, candlestick signals, calculator, logs will be implemented here.</p>
+    </div>
+  );
+}
+
+export default App;
